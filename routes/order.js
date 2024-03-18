0f48d1bd28e3cd480ecdce99d5dcef79f4dbc77f
bassamnegm <bassamnegm@gmail.com>
2024-03-18 23:43:20 +0200

    update get bill

--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,7 @@ const express = require("express");
 const validate = require("../middlewares/validate");
 const orderSchema = require("../utils/validators/orderSchema");
 const routes = express.Router();
-const { createOrder, getAllOrders } = require("../controllers/order");
+const { createOrder, getAllOrders, getOrder } = require("../controllers/order");
 
 routes.get('/get-all', getAllOrders);
 routes.post('/create-order', validate(orderSchema), createOrder);
