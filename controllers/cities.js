76709bb6d275bd40dc78fcca06156425335e1c32
Soha Ali <sohaali710@gmail.com>
2024-07-18 17:02:16 +0300

    Update

--- controllers/cities.js
+++ controllers/cities.js
@@ -1,11 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const citiesData = require("../models/cities.json");
-const districtsData = require("../models/districts.json");
 
 exports.getCities = asyncHandler(async (req, res) => {
   res.status(200).json({ ...citiesData });
-});
-
-exports.getDistricts = asyncHandler(async (req, res) => {
-  res.status(200).json({ ...districtsData });
 });
--- routes/cities.js
+++ routes/cities.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const routes = express.Router();
-const { getCities, getDistricts } = require("../controllers/cities");
+const { getCities } = require("../controllers/cities");
 
 routes.get("/", getCities);
-routes.get("/districts", getDistricts);
 
 module.exports = routes;
