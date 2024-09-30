a272045ebeeab9d8f65bcf0f0f351b03a962e1b7
Soha Ali <sohaali710@gmail.com>
2024-10-01 02:00:02 +0300

    Edit file path

--- insert.js
+++ insert.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 // Read the JSON file
 const data = JSON.parse(
-  fs.readFileSync("/root/logsitics/gotex-logistics/districts.json", "utf8")
+  fs.readFileSync("/root/logsitics-p/gotex-logistics/districts.json", "utf8")
 );
 
 // Connect to the database and insert the data
